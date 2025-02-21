7449271201c7666aff70c207981ddd030200ef47
ljcla <ljclaeyssen@gmail.com>
2025-02-22 00:04:33 +0100

    feat: chall fail ranking

--- src/app/challenge-failer/components/podium/podium.component.ts
+++ src/app/challenge-failer/components/podium/podium.component.ts
@@ -1,13 +1,10 @@
-import {JsonPipe} from '@angular/common';
-import {Component, computed, inject} from '@angular/core';
-import {withComputed} from '@ngrx/signals';
+import {Component, inject} from '@angular/core';
 import {FailersStore} from '../../stores/failers.store';
 import {PodiumElementComponent} from './podium-element/podium-element.component';
 
 @Component({
   selector: 'app-podium',
   imports: [
-    JsonPipe,
     PodiumElementComponent
   ],
   templateUrl: './podium.component.html',
